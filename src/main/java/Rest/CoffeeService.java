8faf76c6695e8240cb45b18d6ece20c7538d247d
StijnB <s.baltessen@studen.fontys.nl>
2018-10-18 14:12:11 +0200

    eventserver

--- src/main/java/Rest/CoffeeService.java
+++ src/main/java/Rest/CoffeeService.java
@@ -3,10 +3,11 @@ package Rest;
 import Controller.CoffeeController;
 import Entities.Product;
 
+import javax.websocket.server.ServerEndpoint;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import java.util.List;
-
+@ServerEndpoint(value = "/coffee")
 @Path("/Coffee")
 public class CoffeeService {
     private CoffeeController coffeeController  = new CoffeeController();
