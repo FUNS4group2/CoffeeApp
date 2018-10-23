07c092f694d1378fbbbadbc93f35a9e84c562fd7
StijnB <s.baltessen@studen.fontys.nl>
2018-10-23 09:51:41 +0200

    order and test added

--- src/main/java/Rest/CoffeeService.java
+++ src/main/java/Rest/CoffeeService.java
@@ -2,19 +2,36 @@ package Rest;
 
 import Controller.CoffeeController;
 import Entities.Product;
+import jdk.nashorn.internal.objects.annotations.Getter;
 
 import javax.websocket.server.ServerEndpoint;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import java.util.List;
-@ServerEndpoint(value = "/coffee")
+
 @Path("/Coffee")
 public class CoffeeService {
     private CoffeeController coffeeController  = new CoffeeController();
 
+//    @GET
+//    @Path("/Coffees")
+//    public List<Product> getCoffees(){
+//        return coffeeController.getAllProducts();
+//    }
+
+    @POST
+    @Path("order")
+    public int order(@FormParam("productId") int porductId,@FormParam("milk") int milk, @FormParam("sugar") int sugar){
+        System.out.println("iets hier");
+        return 1;
+    }
+
     @GET
-    @Path("/Coffees")
-    public List<Product> getCoffees(){
-        return coffeeController.getAllProducts();
+    @Path("test")
+    public String test(){
+        System.out.println("stuff happend");
+        return "asf";
     }
 }
